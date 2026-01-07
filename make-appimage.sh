06c586e70b28fc6b9da41da4bc428b682f641fa9
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-07 14:58:46 -0300

    Update script for Dragon Player application

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,17 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q kaffeine | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q dragon | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/kaffeine.svg
-export DESKTOP=/usr/share/applications/org.kde.kaffeine.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/dragonplayer.svg
+export DESKTOP=/usr/share/applications/org.kde.dragonplayer.desktop
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/kaffeine /usr/lib/vlc
+quick-sharun /usr/bin/dragonplayer /usr/lib/vlc
 
 # Additional changes can be done in between here
 
