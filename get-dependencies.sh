fb91df842a5aa30557d359da30edb690f7d470f4
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-07 15:18:29 -0300

    Update package name from dragon to dragonplayer

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	dragon            \
+	dragonplayer            \
 	kvantum-qt5         \
 	pipewire-audio      \
 	pipewire-jack       \
