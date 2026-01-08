e69acbb515773b67c7a05a2e05e0a30cc523fee9
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-08 14:55:56 -0300

    Refactor make-appimage.sh by removing unused variables
    Removed MAIN_BIN export and ffmpeg from quick-sharun command.

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q dragon | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export MAIN_BIN=/usr/bin/dragon
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/dragonplayer.png
@@ -14,7 +13,7 @@ export DESKTOP=/usr/share/applications/org.kde.dragonplayer.desktop
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/dragon /usr/lib/vlc /usr/bin/ffmpeg
+quick-sharun /usr/bin/dragon /usr/lib/vlc
 
 # Additional changes can be done in between here
 
