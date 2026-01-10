f259c7fa7730c2162496dbf995e9422553672682
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-10 20:14:24 -0300

    Update dependency path in make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DESKTOP=/usr/share/applications/org.kde.dragonplayer.desktop
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/dragon /usr/lib/vlc
+quick-sharun /usr/bin/dragon /usr/lib/qt6
 
 # Additional changes can be done in between here
 
